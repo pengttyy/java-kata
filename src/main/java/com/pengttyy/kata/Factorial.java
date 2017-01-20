f72be85ef30f64497414561686c2e5ac8dec3534
pengttyy <pengttyy@gmail.com>
2017-01-20 17:22:20 +0800

    feat:阶乘处理
    - 如果输入参数大于12抛出异常IllegalArgumentException
    - 如果输入参数小于0抛出异常IllegalArgumentException

---
+++ src/main/java/com/pengttyy/kata/Factorial.java
@@ -0,0 +1,10 @@
+package com.pengttyy.kata;
+
+public class Factorial {
+    public int factorial(int n) {
+        if (n < 0 || n > 12) {
+            throw new IllegalArgumentException();
+        }
+        return n == 0 ? 1 : n * factorial(n - 1);
+    }
+}
---
+++ src/test/java/com/pengttyy/kata/FactorialTests.java
@@ -0,0 +1,49 @@
+package com.pengttyy.kata;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FactorialTests {
+
+    private Factorial fact;
+
+    @Before
+    public void initFactorial() {
+        fact = new Factorial();
+    }
+
+    @After
+    public void afterFactorial() {
+        fact = null;
+    }
+
+    @Test
+    public void test_factorial0() {
+        assertEquals(1, fact.factorial(0));
+    }
+
+    @Test
+    public void test_factorial3() {
+        assertEquals(6, fact.factorial(3));
+    }
+
+    @Test
+    public void test_factorial5() {
+        assertEquals(120, fact.factorial(5));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_factorial6() {
+        fact.factorial(13);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_factorial7() {
+        fact.factorial(-1);
+    }
+
+
+}
