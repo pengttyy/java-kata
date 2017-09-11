8548362f28ec437e2c235961055821f124f8ec27
pengttyy <pengttyy@gmail.com>
2017-09-11 22:13:55 +0800

    fix(leetcode): Longest Substring Without Repeating Characters
    自主实现与最快实现

--- longest-substring-without-repeating-characters/src/main/java/com/pengttyy/Solution.java
+++ longest-substring-without-repeating-characters/src/main/java/com/pengttyy/Solution.java
@@ -8,21 +8,39 @@ public class Solution {
     public int lengthOfLongestSubstring(String str) {
         byte[] bytes = str.getBytes();
         int maxLen = 0;
-        Set<Byte> tempSet = new HashSet<>();
         for (int i = 0; i < bytes.length; i++) {
+            Set<Byte> tempSet = new HashSet<>();
             for (int z = i; z < bytes.length; z++) {
                 if (tempSet.contains(bytes[z])) {
-                    maxLen = Math.max(maxLen, tempSet.size());
-                    tempSet.clear();
                     break;
-                } else if (z == bytes.length - 1) {
-                    maxLen = Math.max(maxLen, tempSet.size() + 1);
-                    tempSet.clear();
-                } else {
-                    tempSet.add(bytes[z]);
                 }
+                tempSet.add(bytes[z]);
             }
+
+            if (bytes.length - i == tempSet.size()) {
+                return Math.max(maxLen, tempSet.size());
+            }
+
+            maxLen = Math.max(maxLen, tempSet.size());
         }
         return maxLen;
+    }
+
+    public int lengthOfLongestSubstring_fast(String s) {
+        if (s.length() == 0) return 0;
+        // for ASCII char sequence, use this as a hashmap
+        int[] idx = new int[160];
+        //initaize arr to -1 inorder to correct calculate interval
+        for (int i = 32; i < idx.length; i++) {
+            idx[i] = -1;
+        }
+        int max = 0, m = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int ascii = (int) (s.charAt(i));
+            m = Math.max(idx[ascii] + 1, m);
+            idx[ascii] = i;
+            max = Math.max(max, i - m + 1);
+        }
+        return max;
     }
 }
