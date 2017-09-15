4e5927272dd53a4a8ebdfe6746b8899b3ca9c284
pengttyy <pengttyy@gmail.com>
2017-09-15 17:49:59 +0800

    fix(leetcode): Median of Two Sorted Arrays

--- median-of-two-sorted-arrays/src/main/java/com/pengttyy/Solution.java
+++ median-of-two-sorted-arrays/src/main/java/com/pengttyy/Solution.java
@@ -1,7 +1,18 @@
 package com.pengttyy;
 
+import java.util.Arrays;
+
 public class Solution {
     public double findMedianSortedArrays(int[] num1, int[] num2) {
-        return 1;
+        int[] all = new int[num1.length + num2.length];
+        System.arraycopy(num1, 0, all, 0, num1.length);
+        System.arraycopy(num2, 0, all, num1.length, num2.length);
+        Arrays.sort(all);
+        int i = all.length / 2;
+        if (all.length % 2 == 0) {
+            return (all[i]+all[i-1])/2.0;
+        } else {
+            return all[i];
+        }
     }
 }
